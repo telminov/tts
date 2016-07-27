2af826aa73750d7ed75f677b02a1818ff5b5f081
bm0 <paul0radkov@gmail.com>
2016-07-27 03:21:18 +0300

    Test for 'get_file' view

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -22,3 +22,17 @@ class TestGenerateView(APITestCase):
     def tearDown(self):
         shutil.rmtree(settings.OUTPUT_DIR)
 
+
+class TestGetFileView(APITestCase):
+    get_url = reverse('Core:get_file')
+    gen_url = reverse('Core:generate')
+
+    def test_get_file(self):
+        gen_response = self.client.post(self.gen_url, {'text': 'Awesome Text!'})
+        gen_content = json.loads(gen_response.content.decode('utf-8'))
+
+        get_response = self.client.get(self.get_url, {'uuid': gen_content['uuid']})
+
+        self.assertEqual(get_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(get_response['Content-Disposition'], 'attachment; filename=%s.wav' % gen_content['uuid'])
+        # TODO Пока неясно, как проверить наличие файла в response'е
